da2c7129d9457fffcc6567cf88af191f9b693313
Ren <ren.t21@icloud.com>
2021-12-14 21:30:01 +0900

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by 高橋蓮 on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
